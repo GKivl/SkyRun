c7b8ae5d69d5ced525654e9ae9e68cc17ca666e3
Gustavs Kivlenieks <139682761+GKivl@users.noreply.github.com>
2024-09-30 18:40:41 +0300

    Added basic movement

--- Assets/TheController.cs
+++ Assets/TheController.cs
@@ -10,41 +10,33 @@ public class TheController : MonoBehaviour {
     public float moveSpeed;
     public float rotateSpeed;
 
-    private Vector2 _look;
-    private Vector2 _move;
-    private Vector2 _rotation;
+    public InputAction moveAction;
+    public InputAction lookAction;
+    
+    private Rigidbody _rb;
+    private Vector3 _moveVector = Vector3.zero;
+    private Vector2 _lookVector = Vector2.zero;
 
-    public void Update() {
-        // Update orientation first, then move. Otherwise, move orientation will lag
-        // behind by one frame.
-        Look(_look);
-        Move(_move);
+    private void Awake() {
+        _rb = GetComponent<Rigidbody>();
+    }
+    void OnEnable() {
+        moveAction.Enable();
+        lookAction.Enable();
     }
 
-    public void OnMove(InputAction.CallbackContext context) {
-        _move = context.ReadValue<Vector2>();
+    void OnDisable() {
+        moveAction.Disable();
+        lookAction.Disable();
     }
 
-    public void OnLook(InputAction.CallbackContext context) {
-        _look = context.ReadValue<Vector2>();
+    void Update() {
+        _moveVector = moveAction.ReadValue<Vector3>();
+        _lookVector = lookAction.ReadValue<Vector2>();
     }
 
-    private void Move(Vector2 direction) {
-        if (direction.sqrMagnitude < 0.01)
-            return;
-        var scaledMoveSpeed = moveSpeed * Time.deltaTime;
-        // For simplicity's sake, we just keep movement in a single plane here. Rotate
-        // direction according to world Y rotation of player.
-        var move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.y);
-        transform.position += move * scaledMoveSpeed;
+    private void FixedUpdate() {
+        _rb.velocity = _moveVector * (moveSpeed * Time.deltaTime);
     }
-
-    private void Look(Vector2 rotate) {
-        if (rotate.sqrMagnitude < 0.01)
-            return;
-        var scaledRotateSpeed = rotateSpeed * Time.deltaTime;
-        _rotation.y += rotate.x * scaledRotateSpeed;
-        _rotation.x = Mathf.Clamp(_rotation.x - rotate.y * scaledRotateSpeed, -89, 89);
-        transform.localEulerAngles = _rotation;
-    }
+    
 }
